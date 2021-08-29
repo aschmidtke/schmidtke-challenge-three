a983eda048b3653e153e4a9632cb4d38f3c4ac19
Drew Schmidtke <amschmidtke@gmail.com>
2021-08-29 18:08:35 -0400

    debugged js

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 
 // prompts - including length - user can choose to create password
-var generatePassword = function () {
-function chooseCriteria() {
+var generatePrompts = function () {
     var passwordLength = prompt("Please choose and enter password length between 8-128 characters:");
     if (passwordLength < 8 || passwordLength > 128) {
-      alert("This is an invalid choice.");
-      return null;
-    } 
+        alert("This is an invalid choice.");
+        return null;
+    }
     var hasNumbers = confirm("Click OK if you would like to inlcude numbers in your password");
     var hasLowercase = confirm("Click OK if you would like to include lowercase letters in your password");
     var hasUppercase = confirm("Click OK if you would like to include uppercase letters in your password");
@@ -26,75 +25,64 @@ function chooseCriteria() {
     }
     return passwordObject;
 }
-chooseCriteria();
 
-
-// We need another function for the random generation of characters
+// random generation
 function randomMechanism(array) {
-    var randomPassword = array[Math.floor(Math.random() * array.passwordLength)]
+    var randomPassword = array[Math.floor(Math.random() * array.length)]
     return randomPassword;
 }
 
-return finalPassword();
-
-// We need another function that actually puts the password together (concat and places into empty array)
-// statement that adds the array of each choice, numbers, letters, characters based on whatever the user has inputted
-// adding usually means using concat
-function finalPassword() {
-    var answers = chooseCriteria();
+// puts password together
+function finalPassword(answers) {
     var final = [];
     var possibleChar = [];
     var confirmChar = [];
 
-    if(answers.hasNumbers) {
+    if (answers.hasNumbers) {
         possibleChar = possibleChar.concat(numbers)
         confirmChar.push(randomMechanism(numbers))
     }
-    if(answers.hasLowercase) {
+    if (answers.hasLowercase) {
         possibleChar = possibleChar.concat(lowercase)
         confirmChar.push(randomMechanism(lowercase))
     }
-    if(answers.hasUppercase) {
+    if (answers.hasUppercase) {
         possibleChar = possibleChar.concat(uppercase)
         confirmChar.push(randomMechanism(uppercase))
     }
-    if(answers.hasSpecial) {
+    if (answers.hasSpecial) {
         possibleChar = possibleChar.concat(special)
         confirmChar.push(randomMechanism(special))
     }
 
-for( var i = 0; i < answers.passwordLength; i++) {
-    var possibleChar = randomMechanism(possibleChar);
-    final.push(possibleChar);
-    
-}
-
-for( var i = 0; i < confirmChar.length; i++) {
-    final[i] = confirmChar[i];
-    
-}
+    for (let i = 0; i < answers.passwordLength; i++) {
+        final.push(randomMechanism(possibleChar));
+    }
 
 
-
-// converts to text format
-return final.join('')
+    // converts to text format
+    return final.join('')
 
 }
+
+
+function generatePassword() {
+    var answers = generatePrompts()
+    const password = finalPassword(answers)
+    writePassword(password)
 }
-generatePassword();
+
+// Write password to the #password input
+function writePassword(password) {
+
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
